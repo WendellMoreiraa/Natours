204bbafb1bce1b827fd3de34cc181f2e0deba1bd
Wendell moreira <wendell.ufpel@gmail.com>
2022-03-25 17:35:08 -0300

    menu

---
+++ script.js
@@ -0,0 +1,18 @@
+const open = document.querySelector('.open').addEventListener('click', function() {
+    var menu = document.querySelector('.menu__container')
+    menu.style.display = 'flex'
+    var body = document.querySelector('body')
+    body.style.overflow = 'hidden'
+    body.style.margin = '0'
+    body.style.padding = '0'
+})
+
+const close = document.querySelector('.close').addEventListener('click', function() {
+    var menu = document.querySelector('.menu__container')
+    menu.style.display = 'none'
+    var body = document.querySelector('body')
+    body.style.overflow = 'visible'
+   
+})
+
+
